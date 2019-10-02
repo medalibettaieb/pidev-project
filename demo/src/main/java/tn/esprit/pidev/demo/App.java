91ea53644f5244568a5072dd7b5ecd6ecaf4e91e
medali.bettaieb@esprit.tn <gogs@fake.local>
2019-10-02 16:09:41 +0100

    netbeans first project

---
+++ demo/src/main/java/tn/esprit/pidev/demo/App.java
@@ -0,0 +1,17 @@
+package tn.esprit.pidev;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
